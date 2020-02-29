53111e033db3e6e736e0d4ca545cddf6a73626fa
ri23 <robertietswaart@gmail.com>
2020-02-29 15:21:28 -0500

    add input file path

--- scripts/merge_fda.py
+++ scripts/merge_fda.py
@@ -4,11 +4,15 @@
 # MIT license
 
 import pandas as pd
+import os
 
-v1_1=pd.read_csv('Compound_FDA_V1_from0_to_299.csv',sep='\t')
-v1_2=pd.read_csv('Compound_FDA_V1_from300_to_999.csv',sep='\t')
-v1_3=pd.read_csv('Compound_FDA_V1_from1000_to_1499.csv',sep='\t')
-v1_4=pd.read_csv('Compound_FDA_V1_from1500_to_2133.csv',sep='\t')
+repo_path = str(sys.argv[1])
+path = os.path.join(repo_path,'data')
+
+v1_1=pd.read_csv(os.path.join(path,'Compound_FDA_V1_from0_to_299.csv'),sep='\t')
+v1_2=pd.read_csv(os.path.join(path,'Compound_FDA_V1_from300_to_999.csv'),sep='\t')
+v1_3=pd.read_csv(os.path.join(path,'Compound_FDA_V1_from1000_to_1499.csv'),sep='\t')
+v1_4=pd.read_csv(os.path.join(path,'Compound_FDA_V1_from1500_to_2133.csv'),sep='\t')
 
 v1_concat=[v1_1,v1_2,v1_3,v1_4]
 v1_concat=pd.concat(v1_concat,ignore_index=True)
